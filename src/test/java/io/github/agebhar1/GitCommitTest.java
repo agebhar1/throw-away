f03b8b213cd16bb7944b7193d42b217af2732087
Andreas Gebhardt <agebhar1@googlemail.com>
2024-01-18 19:28:47 +0100

    add test

---
+++ src/test/java/io/github/agebhar1/GitCommitTest.java
@@ -0,0 +1,32 @@
+package io.github.agebhar1;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GitCommitTest {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            var resource = GitCommitTest.class.getResource("/git.properties");
+            if (resource != null) {
+                try (var stream = resource.openStream()) {
+                    properties.load(stream);
+                }
+            }
+        } catch (IOException ignore) {
+        }
+    }
+
+    @Test
+    public void test() {
+        var gitCommitIdDescribe = properties.getOrDefault("git.commit.id.describe", "unknown-git-rev").toString();
+        assertThat(gitCommitIdDescribe).matches("[0-9a-f]+(-dirty)?");
+    }
+
+}
